63d161a6d045daafd961a50accadc17c25069cd6
Ethan <hobobanana64@gmail.com>
2020-10-19 20:46:57 -0500

    OOP'd input_node

--- robot_ws/src/robot_control/src/input_node.py
+++ robot_ws/src/robot_control/src/input_node.py
@@ -6,42 +6,46 @@ from robot_msgs.msg import coordinate
 from robot_msgs.msg import tasks
 from std_msgs.msg import Bool
 
-def input_callback(data):
-    myTasks = tasks()
+class InputReader:
 
-    if data.data == False:
-        print "Input coordinates:"
-        string_input = raw_input()
-        while string_input != "":
-            
-            temp = re.findall(r'\d+', string_input)
-            nums = list(map(float, temp)) 
+    def __init__(self):
+        self.tasks_pub = rospy.Publisher('/robot/tasks', tasks, queue_size = 10)
+        self.busy_sub = rospy.Subscriber('/robot/busy_bool', Bool, self.input_callback)
 
-            if len(nums) == 4:
-                start_coord = coordinate()
-                end_coord = coordinate()
-                start_coord.x_coord = nums[0]
-                start_coord.y_coord = nums[1]
-                end_coord.x_coord = nums[2]
-                end_coord.y_coord = nums[3]
+    def input_callback(self, data):
+        myTasks = tasks()
 
-                myTasks.coord_list.append(start_coord)
-                myTasks.coord_list.append(end_coord)
-            else:
-                print "Input not valid as (<start_coordinates>, <end_coordinates>)"
+        if data.data == False:
+            rospy.sleep(1)
+            print "Input coordinates:"
             string_input = raw_input()
+            while string_input != "":
+                
+                temp = re.findall(r'\d+', string_input)
+                nums = list(map(float, temp)) 
 
-        tasks_pub.publish(myTasks)
-        rospy.sleep(5)
+                if len(nums) == 4:
+                    start_coord = coordinate()
+                    end_coord = coordinate()
+                    start_coord.x_coord = nums[0]
+                    start_coord.y_coord = nums[1]
+                    end_coord.x_coord = nums[2]
+                    end_coord.y_coord = nums[3]
+
+                    myTasks.coord_list.append(start_coord)
+                    myTasks.coord_list.append(end_coord)
+                else:
+                    print "Input not valid as (<start_coordinates>, <end_coordinates>)"
+                string_input = raw_input()
+
+            self.tasks_pub.publish(myTasks)
+            rospy.sleep(5)
 
 def init_input_node():
     rospy.init_node('input_node', anonymous = False)
     rate = rospy.Rate(10)
 
-    global tasks_pub
-
-    tasks_pub = rospy.Publisher('/robot/tasks', tasks, queue_size = 10)
-    rospy.Subscriber('/robot/busy_bool', Bool, input_callback)
+    reader = InputReader()
 
     rospy.spin()
 
